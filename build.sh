fefdbf8066e54b107b3ab28c37e6f931ca69093e
Denis <denis.eiras@gmail.com>
2024-02-07 10:56:54 -0300

    spack load

--- build.sh
+++ build.sh
@@ -2,9 +2,10 @@
 #
 # Author: Larissa Reames CIWRO/NOAA/NSSL/FRDD
 
-set -eux
+#set -eux
 
-target=${1:-"NULL"}
+#target=${1:-"NULL"}
+target="egeon.intel"
 compiler=${compiler:-"intel"}
 echo $target, $compiler
 if [[ "$target" == "linux.*" || "$target" == "macosx.*" ]]; then
@@ -12,6 +13,17 @@ if [[ "$target" == "linux.*" || "$target" == "macosx.*" ]]; then
     set +x
     source ./modulefiles/build.$target > /dev/null
     set -x
+elif [ "$target" == "egeon.intel" ]; then
+    echo -e "\n\nConfiguring Egeon ...\n======================"
+    echo -e "\n\nLoading spack env ..."
+    source ../MPASSIT_spack/spack_mpassit/env.sh
+    
+    echo -e "\n\nLoading all spack packages installed ..."
+    spack load
+    echo -e "\n\nAll spack packages installed:"
+    spack find
+    echo -e "\n\nShowing all modules loaded:"
+    module list
 else
     set +x
     source ./machine-setup.sh
@@ -26,15 +38,14 @@ if [[ "$target" == "hera" || "$target" == "orion" || "$target" == "wcoss2" || "$
    CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DBUILD_TESTING=OFF"
    #CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DENABLE_DOCS=ON -DBUILD_TESTING=ON"
 else
-   CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Debug"
-   #CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DBUILD_TESTING=OFF"
+   #CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Debug"
+   CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=../ -DEMC_EXEC_DIR=ON -DBUILD_TESTING=OFF"
 fi
 
 rm -fr ./build
 mkdir ./build && cd ./build
 
 cmake .. ${CMAKE_FLAGS}
-
 make -j 8 VERBOSE=1
 make install
 
